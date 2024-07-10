86cc9094a1f73f4573aae22dfeb074477bb4af88
HARSH YADAV <149269219+HARSH-4545@users.noreply.github.com>
2024-07-11 02:55:17 +0530

    Create  FlowLayoutExample.java

---
+++ FlowLayoutExample.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class FlowLayoutExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("FlowLayout Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 20));
+        panel.add(new JButton("Button 1"));
+        panel.add(new JButton("Button 2"));
+        panel.add(new JButton("Button 3"));
+        panel.add(new JButton("Button 4"));
+        panel.add(new JButton("Button 5"));
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
