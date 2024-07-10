b0a74fa0a10f9d62d5c072d4a7ff6391ea51a2ee
HARSH YADAV <149269219+HARSH-4545@users.noreply.github.com>
2024-07-11 02:52:45 +0530

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    // Default constructor
+    public MyClass() {
+        System.out.println("Default constructor called");
+    }
+
+    public static void main(String[] args) {
+        MyClass obj = new MyClass(); // Default constructor is called
+    }
+}
